79877fe9eccb6034078c2b5cdbaa0fe5dc86c90a
pagarevijayy <pagarevj97@gmail.com>
2021-04-01 23:56:35 +0530

    material + side-nav raw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
+// package imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+// internal imports
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModuleModule } from './material.module';
 import { environment } from '../environments/environment';
+
+// components
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModuleModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
